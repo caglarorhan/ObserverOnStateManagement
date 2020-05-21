333a4a36af6f6088ced59bf0deda2bff77bb24c2
caglarorhan <caglaror@gmail.com>
2020-05-21 17:35:37 -0500

    refactore the observerOnState function

--- observerOnState.js
+++ observerOnState.js
@@ -1,44 +1,33 @@
-const state = {};
-
-function objectReMake(targetObject){
-    if(typeof targetObject === 'object'){
-        if(!targetObject.subsList){
-            targetObject["subsList"]=[];
-            targetObject["subscribeMe"]= function(subs){this.subsList.push(subs); return true;}
-            targetObject["unSubscribeMe"] = function(subs){
-                while(this.subsList.indexOf(subs)>-1){
-                    this.subsList.splice(this.subsList.indexOf(subs),1)
-                }
-                return true;
-            }
-        }
-        Object.defineProperty(targetObject,'tempProp',{
-            set(updatedData){
-                Object.entries(updatedData).forEach((entry)=>{
-                    let key = entry[0];
-                    let value = entry[1];
-                    targetObject[key]=value;
-                    if(typeof value==='object'){
-                        objectReMake(value);
+function observerOnState(targetObject){
+    for (const key in targetObject) {
+        if (targetObject.hasOwnProperty(key) && targetObject[key] instanceof Object && targetObject[key].constructor===Object && !Array.isArray(targetObject[key])) {
+            let value = targetObject[key];
+            if(!value.subsList){
+                value["subsList"]=[];
+                value["subscribeMe"]= function(subs){this.subsList.push(subs)};
+                value["unSubscribeMe"] = function(subs){
+                    while(value.subsList.indexOf(subs)>-1){
+                        value.subsList.splice(value.subsList.indexOf(subs),1)
                     }
-                 });
-                if(targetObject.subsList){
-                    targetObject.subsList.forEach((item)=>{
-                        console.log(`Publish to: ${item}`);// publisher comes to here
-                    });
                 }
             }
-        })
+            Object.defineProperty(value, 'tmp',{
+                set(v){
+                    if(value.subsList){
+                        value.subsList.forEach((item)=>{
+                            console.log(`Publish to: ${item}`);// publisher comes to here
+                        });
+                    }
+                }
+            })
+            observerOnState(value);
+        }
     }
 }
 
-//console.log(state);
-objectReMake(state); //to initiate setter and other methods
-state.tempProp={userInfo:{hair:'long', eye:'brown'}}; // tempProp is to trigging property change mechanism (setter)
-state.userInfo.tempProp={hair:'long', eye:'blue'}
-state.userInfo.tempProp={hair:'long', eye: 'brown', beard:{type:'goat', length:{inch:2}}, category:{hair:'Johny', mustache:{type:'Texan', length:22}}};
-// did not add subList into length property depth after 2 child is not affected
-//state.userInfo.category.tempProp = {hair:'Johny', mustache:'Texas', eyebrows:'Mellow'}
+const state ={userInfo:{hair:'long', eye: 'brown', beard:{type:'goat', length:{inch:2}}, category:{face:'Johny', mustache:{tip:'Texan', length:22}}}};
+
+observerOnState(state);
+state.userInfo.category.mustache.subscribeMe('Berberler');
 console.log(JSON.stringify(state));
-
-
+state.userInfo.category.mustache.tmp={tip:'Mexican', length:22};
